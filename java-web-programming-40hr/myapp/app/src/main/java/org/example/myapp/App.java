dbb4d55abc1ce856625b465a5d0d0a536833c14e
cluejws <cluejws@gmail.com>
2025-11-13 14:52:33 +0900

    #day2 set i/o app

--- java-web-programming-40hr/myapp/app/src/main/java/org/example/myapp/App.java
+++ java-web-programming-40hr/myapp/app/src/main/java/org/example/myapp/App.java
@@ -3,12 +3,18 @@
  */
 package org.example.myapp;
 
+import java.util.Scanner;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("이름: ");
+        String name = scanner.nextLine();
+        System.out.printf("안녕하세요 %s 님!\n", name);
+        scanner.close();
     }
 }
