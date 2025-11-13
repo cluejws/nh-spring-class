e3259e2973dad44f430171dc867bcaa11b56fdd4
cluejws <cluejws@gmail.com>
2025-11-13 15:45:52 +0900

    #day2 init spring boot web project

--- java-web-programming-40hr/myapp/app/src/main/java/org/example/myapp/App.java
+++ java-web-programming-40hr/myapp/app/src/main/java/org/example/myapp/App.java
@@ -3,18 +3,13 @@
  */
 package org.example.myapp;
 
-import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("이름: ");
-        String name = scanner.nextLine();
-        System.out.printf("안녕하세요 %s 님!\n", name);
-        scanner.close();
+        SpringApplication.run(App.class, args);
     }
 }
